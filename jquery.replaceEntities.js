166a88191def10740de68de97dfb477e189db6f2
Michael Lawrence <michael.lawrence@rhythmagency.com>
2013-03-19 16:55:36 -0700

    Updating to match jquery.pluginTemplate updates.

--- jquery.replaceEntities.js
+++ jquery.replaceEntities.js
@@ -12,7 +12,7 @@
 
 		exports[plugin.name] = plugin.fn;
 	} else if (typeof define === 'function' && define.amd) { // AMD
-		define(['jquery'], function ($) {
+		define(plugin.deps, function ($) {
 			var fn = (plugin.isStatic ? $ : $.fn)[plugin.name] = plugin.factory($);
 			return fn;
 		});
@@ -27,6 +27,7 @@
 })(window.$, {
 	'name' : 'replaceEntities',
 	'isStatic' : true,
+	'deps' : ['jquery'],
 	'factory' : function ($) {
 		return function () {
 			var decoded = $(this).html().replace(/(\&\w+\;)/g, function(match, $1) {
